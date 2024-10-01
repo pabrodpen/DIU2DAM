fbedd50552e682f9f0d1263d8ba6120fc5c19145
pabrodpen <pabrodpen@gmail.com>
2024-10-01 10:39:57 +0200

    add

--- EjemplosComponentes/src/main/java/com/example/ejemplos/ContadorEstilosInteraccion.java
+++ EjemplosComponentes/src/main/java/com/example/ejemplos/ContadorEstilosInteraccion.java
@@ -2,6 +2,9 @@ package com.example.ejemplos;
 
 import javafx.application.Application;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -15,16 +18,119 @@ import javafx.stage.Stage;
 public class ContadorEstilosInteraccion extends Application {
     Label lbNumero;
     Button bSumar,bRestar,bCero;
-    int numPulsaciones=1;
+    private IntegerProperty numPulsaciones = new SimpleIntegerProperty(1);
 
-    private void pulsado(int n){
-        numPulsaciones=n+numPulsaciones;
+
+    public void pulsado(int n){
         if(n==0){
-            numPulsaciones=0;
+            numPulsaciones.set(0);
         }
-        lbNumero.setText(String.valueOf(numPulsaciones));
+        numPulsaciones.set(numPulsaciones.get()+n);
 
     }
+
+    public IntegerProperty getNumPulsaciones(){
+        return numPulsaciones;
+    }
+
+    public VBox crearPantalla(){
+
+
+        HBox botones=new HBox();
+
+        botones.setPadding(new Insets(10));
+        botones.setSpacing(10);
+        botones.setAlignment(Pos.CENTER);
+
+
+
+        bSumar=new Button();
+        bRestar=new Button();
+        bCero=new Button();
+        bSumar.setId("bSumar");
+        bRestar.setId("bRestar");
+        bCero.setId("bCero");
+
+
+        bSumar.setText("+");
+        bRestar.setText("-");
+        bCero.setText("0");
+
+        bSumar.setOnAction(event->pulsado(1));
+
+        bRestar.setOnAction(event->pulsado(-1));
+
+        bCero.setOnAction(event->pulsado(0));
+
+        botones.getChildren().addAll(bSumar,bRestar,bCero);
+
+
+        lbNumero=new Label();
+
+        lbNumero.setFont(Font.font("Ani", 40));
+        lbNumero.setMinWidth(100);
+        lbNumero.setAlignment(Pos.CENTER);
+        // Asignar ID a la etiqueta para el CSS
+        lbNumero.setId("lbNumero");
+
+        // Inicialmente mostrar el valor de numPulsaciones en el Label
+        lbNumero.setText(String.valueOf(numPulsaciones.get()));
+
+
+        // Listener para actualizar la etiqueta cuando cambia
+        // el valor del contador
+        numPulsaciones.addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                numPulsaciones.addListener((observable, oldValue, newValue) -> {
+                    lbNumero.setText(String.valueOf(newValue));
+                });
+
+            }
+        });
+
+        /*
+        * El método bind
+        * establece una vinculación unidireccional entre la propiedad
+        * textProperty de lbNumero y otra propiedad (en este caso, numPulsaciones)
+        * . Esto significa que si numPulsaciones cambia, el valor de textProperty
+        * se actualizará automáticamente para reflejar ese cambio.
+        * */
+
+
+
+
+        VBox raiz=new VBox();
+
+        raiz.setPadding(new Insets(10));
+        raiz.setSpacing(10);
+        raiz.setAlignment(Pos.CENTER);
+
+        //coger la clase para el css
+        raiz.getStyleClass().add("raiz");
+
+
+
+        raiz.getChildren().addAll(botones,lbNumero);
+
+
+        /*solo es crear los hbox y vbox, no hace falta vvcrear la escena
+        Scene escena = new Scene(raiz, 420, 150);
+        escena.getStylesheets().add(getClass().getResource("/styles/estilosContador.css").toExternalForm());
+        stage.setTitle("Contador");
+        stage.setScene(escena);
+        stage.show();*/
+
+        /*
+         *numPulsaciones.asString(): numPulsaciones es un IntegerProperty,
+         * que es una clase especializada de JavaFX para gestionar
+         * propiedades numéricas observables. El método asString()
+         * */
+
+        return raiz;
+
+    }
+
 
 
 
--- EjemplosComponentes/src/main/java/com/example/ejemplos/ContadorProperties.java
+++ EjemplosComponentes/src/main/java/com/example/ejemplos/ContadorProperties.java
@@ -17,118 +17,23 @@ import org.kordamp.bootstrapfx.scene.layout.Panel;
 import static javafx.application.Application.launch;
 
 public class ContadorProperties extends Application {
-    ContadorEstilosInteraccion contador1;
-    ContadorEstilosInteraccion contador2;
-    Label lbNumero;
-    Button bSumar,bRestar,bCero;
-    private IntegerProperty numPulsaciones = new SimpleIntegerProperty(1);
-
-    private void pulsado(int n){
-        if(n==0){//0
-            numPulsaciones.set(0);
-        }else{
-            numPulsaciones.set(numPulsaciones.get()+n);//suma o resta
-        }
-        lbNumero.setText(String.valueOf(numPulsaciones));
-
-    }
-
-    public VBox crearPantalla(){
-
-
-        HBox botones=new HBox();
-
-        botones.setPadding(new Insets(10));
-        botones.setSpacing(10);
-        botones.setAlignment(Pos.CENTER);
-
-
-
-        bSumar=new Button();
-        bRestar=new Button();
-        bCero=new Button();
-        bSumar.setId("bSumar");
-        bRestar.setId("bRestar");
-        bCero.setId("bCero");
-
-
-        bSumar.setText("+");
-        bRestar.setText("-");
-        bCero.setText("0");
-
-        bSumar.setOnAction(event->pulsado(1));
-
-        bRestar.setOnAction(event->pulsado(-1));
-
-        bCero.setOnAction(event->pulsado(0));
-
-        botones.getChildren().addAll(bSumar,bRestar,bCero);
-
-
-        lbNumero=new Label();
-
-        lbNumero.setFont(Font.font("Ani", 40));
-        lbNumero.setMinWidth(100);
-        lbNumero.setAlignment(Pos.CENTER);
-        // Asignar ID a la etiqueta para el CSS
-        lbNumero.setId("lbNumero");
-
-
-        //IMP
-        // Vincular el texto de la etiqueta con la propiedad numPulsaciones
-        lbNumero.textProperty().bind(numPulsaciones.asString());
-
-        /*
-        * El método bind
-        * establece una vinculación unidireccional entre la propiedad
-        * textProperty de lbNumero y otra propiedad (en este caso, numPulsaciones)
-        * . Esto significa que si numPulsaciones cambia, el valor de textProperty
-        * se actualizará automáticamente para reflejar ese cambio.
-        * */
-
-
-
-        VBox raiz=new VBox();
-
-        raiz.setPadding(new Insets(10));
-        raiz.setSpacing(10);
-        raiz.setAlignment(Pos.CENTER);
-
-        //coger la clase para el css
-        raiz.getStyleClass().add("raiz");
-
-
-
-        raiz.getChildren().addAll(botones,lbNumero);
-
-
-        /*solo es crear los hbox y vbox, no hace falta vvcrear la escena
-        Scene escena = new Scene(raiz, 420, 150);
-        escena.getStylesheets().add(getClass().getResource("/styles/estilosContador.css").toExternalForm());
-        stage.setTitle("Contador");
-        stage.setScene(escena);
-        stage.show();*/
-
-        /*
-        *numPulsaciones.asString(): numPulsaciones es un IntegerProperty,
-        * que es una clase especializada de JavaFX para gestionar 
-        * propiedades numéricas observables. El método asString()
-         * */
-
-        return raiz;
-
-    }
-
-
+    ContadorEstilosInteraccion contador1=new ContadorEstilosInteraccion();
+    ContadorEstilosInteraccion contador2=new ContadorEstilosInteraccion();
 
     public void start(Stage stage) throws Exception {
 
         try{
 
+            //IMP
+            /*Sincronizar numPulsaciones de ambos contadores*/
+
+            contador1.getNumPulsaciones().bindBidirectional(contador2.getNumPulsaciones());
+
+            //creamos la escena
 
             Stage stage2=new Stage();
-            VBox raiz1=crearPantalla();
-            VBox raiz2=crearPantalla();
+            VBox raiz1=contador1.crearPantalla();
+            VBox raiz2=contador2.crearPantalla();
 
             //2 escenas
 
