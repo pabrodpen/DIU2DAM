5d5dd3b7c2433a188d3bd1947462ca5338673eb6
pabrodpen <pabrodpen@gmail.com>
2024-10-15 11:09:47 +0200

    add

---
+++ AppConversor/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.appconversor {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires AccesoBBDDMoneda;
+
+    opens com.example.appconversor to javafx.fxml;
+    exports com.example.appconversor;
+}
