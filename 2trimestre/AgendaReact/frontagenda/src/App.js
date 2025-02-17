a60e0d656dd231d8ceb32300b73782e21c468ffd
pabrodpen <pabrodpen@gmail.com>
2025-02-17 11:27:09 +0100

    add

--- 2trimestre/AgendaReact/frontagenda/src/App.js
+++ 2trimestre/AgendaReact/frontagenda/src/App.js
@@ -1,50 +1,53 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React, { useRef } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import PersonsList from "./components/persons.list";
 import AddPerson from "./components/add.person";
-import EditPerson from "./components/edit.person"
+import EditPerson from "./components/edit.person";
 
+function App() {
+  const personsListRef = useRef(null); // Creamos la referencia
 
-class App extends Component {
-  render() {
-    const { removeAllPersons } = this.props;
-    return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-                <div className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link to={"/persons"} className="nav-link">
-                  Personas
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={"/add"} className="nav-link">
-                  Add
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <button className="nav-link btn btn-link" onClick={PersonsList.removeAllPersons}>
-                  Limpiar contactos
-                  </button>
-                </li>
-                </div>
-              </nav>
+  const handleRemoveAllPersons = () => {
+    if (personsListRef.current) {
+      personsListRef.current.removeAllPersons(); // Llamamos correctamente la función
+    }
+  };
 
-              <div className="container mt-3">
-                {/*El en switch se renderizarán todas los compoentes cuta URL coicidan con la activa*/}
-          <Routes>
-            <Route path="/" element={<PersonsList />} />
-            <Route path="/persons" element={<PersonsList />} />
-            <Route path="/add" element={<AddPerson/>} />
-            <Route path="/edit/:id" element={<EditPerson/>} /> 
-          </Routes>
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/persons"} className="nav-link">
+              Personas
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+          <li className="nav-item">
+            <button className="nav-link btn btn-link" onClick={handleRemoveAllPersons}>
+              Limpiar contactos
+            </button>
+          </li>
         </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/" element={<PersonsList ref={personsListRef} />} />
+          <Route path="/persons" element={<PersonsList ref={personsListRef} />} />
+          <Route path="/add" element={<AddPerson />} />
+          <Route path="/edit/:id" element={<EditPerson />} />
+        </Routes>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- 2trimestre/AgendaReact/frontagenda/src/components/persons.list.js
+++ 2trimestre/AgendaReact/frontagenda/src/components/persons.list.js
@@ -1,201 +1,127 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, forwardRef, useImperativeHandle } from "react";
 import PersonDataService from "../services/agenda.service";
 import { Link } from "react-router-dom";
 
-export default class PersonsList extends Component {
-  constructor(props) {
-    super(props);
+const PersonsList = forwardRef((props, ref) => {
+  const [persons, setPersons] = useState([]);
+  const [currentPerson, setCurrentPerson] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [searchByName, setSearchByName] = useState("");
 
-    this.state = {
-      persons: [],
-      currentPerson: null,
-      currentIndex: -1,
-      searchByName: ""
-    };
-  }
+  useEffect(() => {
+    retrievePersons();
+  }, []);
 
-  componentDidMount = () => {
-    this.retrievePersons();
-  };
-
-  onChangeSearchByName = (e) => {
-    this.setState({ searchByName: e.target.value });
-  };
-
-  retrievePersons = () => {
+  const retrievePersons = () => {
     PersonDataService.getAll()
-      .then(response => {
-        this.setState({ persons: response.data });
+      .then((response) => {
+        setPersons(response.data);
         console.log(response.data);
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
   };
 
-  refreshList = () => {
-    this.retrievePersons();
-    this.setState({
-      currentPerson: null,
-      currentIndex: -1
-    });
+  const refreshList = () => {
+    retrievePersons();
+    setCurrentPerson(null);
+    setCurrentIndex(-1);
   };
 
-  setPerson = (person, index) => {
-    this.setState({
-      currentPerson: person,
-      currentIndex: index
-    });
-  };
-
-  removeAllPersons = () => {
+  const removeAllPersons = () => {
     PersonDataService.deleteAll()
-      .then(response => {
+      .then((response) => {
         console.log("Contactos eliminados:", response.data);
-        this.refreshList(); // Recargar la lista después de eliminar todos los contactos
+        refreshList(); // Recargar la lista después de eliminar todos los contactos
       })
-      .catch(e => {
+      .catch((e) => {
         console.error("Error al eliminar contactos:", e);
       });
   };
-  
 
-  removePerson = (id) => {
-    PersonDataService.delete(id)
-      .then(response => {
-        console.log(response.data);
-        this.refreshList();
-      }
-      )
-    };
+  useImperativeHandle(ref, () => ({
+    removeAllPersons, // Expone la función removeAllPersons() a App.js
+  }));
 
-  searchByName = () => {
-    PersonDataService.findByName(this.state.searchByName)
-      .then(response => {
-        this.setState({ persons: response.data });
-        console.log(response.data);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
-
-
-
-  render() {
-    console.log(this.state.persons);
-    const { searchByName, persons, currentPerson, currentIndex } = this.state;
-
-    return (
-      <div className="list row">
-        <div className="col-md-8">
-          <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Buscar por nombre"
-              value={searchByName}
-              onChange={this.onChangeSearchByName}
-            />
-            <div className="input-group-append">
-              <button
-                className="btn btn-outline-secondary"
-                type="button"
-                onClick={this.searchByName}
-              >
-                Search
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <h4>Lista de contactos</h4>
-          <ul className="list-group">
-            {persons &&
-              persons.map((person, index) => (
-                <li
-                  className={
-                    "list-group-item " + (index === currentIndex ? "active" : "")
-                  }
-                  onClick={() => this.setPerson(person, index)}
-                  key={index}
-                >
-                  {person.nombre} {person.apellidos}
-                </li>
-              ))}
-          </ul>
-        </div>
-        <div className="col-md-6">
-          {currentPerson ? (
-            <div>
-              <h4>Contacto</h4>
-              <div>
-                <label>
-                  <strong>Nombre:</strong>
-                </label>{" "}
-                {currentPerson.nombre}
-              </div>
-              <div>
-                <label>
-                  <strong>Apellido:</strong>
-                </label>{" "}
-                {currentPerson.apellidos}
-              </div>
-              <div>
-                <label>
-                  <strong>Direccion:</strong>
-                </label>{" "}
-                {currentPerson.calle}
-              </div>
-              <div>
-                <label>
-                  <strong>Localidad:</strong>
-                </label>{" "}
-                {currentPerson.ciudad}
-              </div>
-              <div>
-                <label>
-                  <strong>Codigo postal:</strong>
-                </label>{" "}
-                {currentPerson.codigoPostal}
-              </div>
-              <div>
-                <label>
-                  <strong>Fecha de nacimiento:</strong>
-                </label>{" "}
-                {currentPerson.fechaNacimiento}
-
-                <Link
-                //Como hemos incluido en el switch esta ruta, /tutorials/+id se renderizará el componente
-                // tutorials cuando se pulse el enlace.
-                to={"/agenda/" + currentPerson.id}
-                className="badge badge-warning">
-                Edit
-              </Link>
-              </div>
-
-              <Link to={"/edit/" + currentPerson.id} className="nav-link">
-                <button className="btn btn-primary mt-3" onClick={this.addPerson}>
-                Editar Contacto
-                </button>
-              </Link>
-
-              
-                <button className="btn btn-primary mt-3" onClick={() => this.removePerson(currentPerson.id)}>
-                Eliminar Contacto
-                </button>
-              
-
-              
-            </div>
-          ) : (
-            <div>
-              <br />
-              <p>Selecciona un contacto</p>
-            </div>
-          )}
+  return (
+    <div className="list row">
+      <div className="col-md-8">
+        <div className="input-group mb-3">
+          {/*
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Buscar por nombre"
+            value={searchByName}
+            onChange={(e) => setSearchByName(e.target.value)}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={() => PersonDataService.findByName(searchByName)}
+            >
+              Buscar
+            </button>
+          </div>*/}
         </div>
       </div>
-    );
-  }
-}
+      <div className="col-md-6">
+        <h4>Lista de contactos</h4>
+        <ul className="list-group">
+          {persons &&
+            persons.map((person, index) => (
+              <li
+                className={"list-group-item " + (index === currentIndex ? "active" : "")}
+                onClick={() => {
+                  setCurrentPerson(person);
+                  setCurrentIndex(index);
+                }}
+                key={index}
+              >
+                {person.nombre} {person.apellidos}
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="col-md-6">
+        {currentPerson ? (
+          <div>
+            <h4>Contacto</h4>
+            <div>
+              <label>
+                <strong>Nombre:</strong>
+              </label>{" "}
+              {currentPerson.nombre}
+            </div>
+            <div>
+              <label>
+                <strong>Apellido:</strong>
+              </label>{" "}
+              {currentPerson.apellidos}
+            </div>
+
+            <Link to={"/edit/" + currentPerson.id} className="nav-link">
+              <button className="btn btn-primary mt-3">Editar Contacto</button>
+            </Link>
+
+            <button
+              className="btn btn-danger mt-3"
+              onClick={() => PersonDataService.delete(currentPerson.id).then(refreshList)}
+            >
+              Eliminar Contacto
+            </button>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Selecciona un contacto</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+});
+
+export default PersonsList;
